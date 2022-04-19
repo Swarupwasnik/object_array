8b3981b8678d049e514946c627d3036c585f6327
Swarupwasnik <swarupwasnik13@gmail.com>
2022-04-20 00:15:46 +0530

    object_array

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let studentRecords = [
           {name: 'yaga', id: 200, marks : 45 },
           {name: 'Wick', id: 115, marks : 75 } ] ;
 
-    
+    //Ques1
 
           let names = []
 studentRecords.forEach( student => {
@@ -14,7 +14,7 @@ studentRecords.forEach( student => {
 })
 console.log(names);
 
-
+//QUES2
  let a = studentRecords.filter(student => student.marks > 50);
 console.log(a);
 
@@ -24,10 +24,11 @@ console.log(a);
  let c = studentRecords.reduce( ((acc,emp) => acc+emp.marks), 0);
  console.log(c);
 
+//QUES6
 
  let d = studentRecords.filter(student => stu.id > 120).reduce((acc,curr) => acc + curr.marks ,0)
 console.log(d);
-
+//QUES 4,5,7
 let totalMarks = studentRecords.map(function(stu){ 
      if(student.marks < 50){
          student.marks += 15;}
@@ -35,12 +36,5 @@ let totalMarks = studentRecords.map(function(stu){
  }).filter(stu => student.marks > 50).reduce((acc,curr) => acc+curr.marks, 0);
 console.log(totalMarks);
 
-let arry=[
-     {Name:'Ketan KUbar',rollNo:25},
-     {Name:'Rahul marke',rollNo:25},
-     {Name:'Sikal Umar',rollNo:25},
-     {Name:'Sukumar Kmurr',rollNo:25},
-     {Name:'twinkle KUmr',rollNo:25},
-     {Name:'ram Kurk',rollNo:25}
 
-]
+     
